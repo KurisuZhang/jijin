82e471a36287702d40a9f4c7715b38145a9884dc
LinCheungS <33788131+LinCheungS@users.noreply.github.com>
2021-01-26 15:12:06 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import numpy as np
+import requests
+import json
+import os
+import re
 
 x = np.array([1,2,3,4,5,6])
 
 print(np.exp(x))
+
+data = {"text": "今天基金结果","desp":"hahahahahah"}
+url = "https://sc.ftqq.com/" + env_dist["SERVERJIANG"] + ".send"
+requests.post(url,params=data)
